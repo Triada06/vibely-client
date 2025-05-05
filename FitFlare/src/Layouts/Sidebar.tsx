45216f24b64f81f5db7c6d195b0b9b6721067938
Nizami <tnizami006@gmail.com>
2025-05-05 23:01:47 +0400

    sidebar design finished

--- FitFlare/src/Layouts/Sidebar.tsx
+++ FitFlare/src/Layouts/Sidebar.tsx
@@ -1,27 +1,55 @@
 import { Link, NavLink } from "react-router-dom";
-
+import { motion, AnimatePresence } from "framer-motion";
 import { GridIcon, UserCircleIcon, MoreDotIcon } from "../admin/icons";
+import { useEffect, useRef, useState } from "react";
+import {
+  faSun,
+  faMoon,
+  faGear,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Sidebar() {
+  const [visible, setVisible] = useState(false);
+  const buttonRef = useRef<HTMLDivElement>(null);
+
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const stored = localStorage.getItem("theme");
+    if (stored) setTheme(stored);
+    document.documentElement.classList.toggle("dark", stored === "dark");
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.classList.toggle("dark", newTheme === "dark");
+  };
+
   return (
     <>
-      <aside className="hidden md:flex md:w-82 md:flex-col bg-[#F5F7FA] dark:bg-[#2A2A2D] shadow-[0_4px_20px_rgba(0,0,0,0.1)] rounded-xl  fixed h-full">
+      <aside className=" hidden md:flex md:w-82 md:flex-col bg-[#F5F7FA] dark:bg-[#2A2A2D] shadow-[0_4px_20px_rgba(0,0,0,0.1)] rounded-xl fixed h-full">
         <div className="icon size-10 flex justify-start items-center w-10/12 m-4 ">
           <Link
             to={"/"}
-            className=" size-10 flex justify-start items-center w-10/12  "
+            className="size-10 flex justify-start items-center w-10/12"
           >
-            <img className="size-14" src="public\icon.svg" alt="logo" />
-            <span className="text-[#2E2E2E] dark:text-[#EAEAEA] text-2xl">
+            <img className="size-14" src="public/icon.svg" alt="logo" />
+            <span className="prevent-select text-[#2E2E2E] dark:text-[#EAEAEA] text-2xl">
               Fitflare
             </span>
           </Link>
         </div>
-        <div className="flex items-start m-3 border-b-1 border-[#D3D3D3] text-xl text-[#2E2E2E] dark:text-[#EAEAEA]">
+
+        <div className="flex items-start m-3 border-b border-[#D3D3D3] text-xl text-[#2E2E2E] dark:border-[#3C3C3F] dark:text-[#EAEAEA]">
           <h6>Menu</h6>
         </div>
-        <nav className="flex items-center justify-center  w-full">
-          <ul className=" h-8/10 flex justify-center flex-col w-full">
+
+        <nav className="flex-grow prevent-select ">
+          <ul className="flex flex-col justify-start w-full">
             <li className="m-4 h-2/3">
               <NavLink
                 to="/"
@@ -54,11 +82,73 @@ export default function Sidebar() {
             </li>
           </ul>
         </nav>
-        <section className="m-4 hover:cursor-pointer hover:brightness-110 border-[#D3D3D3] border-t-1 dark:	border-[#3C3C3F]">
-          <div className=" dark:text-[#EAEAEA] h-10 flex items-center justify-start  dark:hover:bg-[#B794F4] dark:hover:text-[#2A2A2D]  text-[#2E2E2E] transition-all hover:rounded-xl duration-300 ease-in-out hover:bg-[#4B3F72] hover:text-[#EAF2EF] mt-1">
-            <MoreDotIcon className="text-2xl transition-all hover:rounded-xl duration-300  dark:hover:text-[#2A2A2D]  " />
+
+        <section className="relative prevent-select  w-full mt-auto mb-4 p-2 ">
+          <div
+            ref={buttonRef}
+            onClick={() => setVisible(!visible)}
+            className="w-full hover:cursor-pointer dark:text-[#EAEAEA] h-10 flex items-center justify-start dark:hover:bg-[#B794F4] dark:hover:text-[#2A2A2D] text-[#2E2E2E] transition-all hover:rounded-xl duration-300 ease-in-out hover:bg-[#4B3F72] hover:text-[#EAF2EF]"
+          >
+            <MoreDotIcon className="text-2xl mr-2" />
             <h6 className="text-xl">More</h6>
           </div>
+
+          <AnimatePresence>
+            {visible && (
+              <motion.div
+                initial={{ opacity: 0, y: 10, scale: 0.95 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                exit={{ opacity: 0, y: 10, scale: 0.95 }}
+                transition={{ duration: 0.2, ease: "easeInOut" }}
+                className="absolute left-0 bottom-full  mb-2 z-50 rounded-xl w-full shadow-xl dark:shadow-[0_4px_20px_rgba(255,255,255,0.1)] shadow-[0_4px_20px_rgba(0,0,0,0.1)] bg-white dark:bg-[#1C1C1E] text-[#2E2E2E] dark:text-[#EAEAEA]"
+              >
+                <ul className="m-4">
+                  <li>
+                    <button
+                      onClick={toggleTheme}
+                      className="w-full flex items-center  px-4 py-2 text-lg transition-all duration-300 rounded-xl
+              hover:bg-[#4B3F72] hover:text-[#EAF2EF] dark:hover:bg-[#B794F4] dark:hover:text-[#2A2A2D]"
+                    >
+                      <FontAwesomeIcon
+                        className="mr-2"
+                        icon={theme === "dark" ? faSun : faMoon}
+                      />
+                      Toggle Theme
+                    </button>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/settings"
+                      onClick={() => setVisible(false)}
+                      className={({ isActive }) =>
+                        `flex items-center px-4 py-2 text-lg transition-all duration-300 rounded-xl w-full ${
+                          isActive
+                            ? "text-[#E07A5F] bg-[#f0f0f0] dark:bg-[#4B3F72] dark:text-[#4DD0E1]"
+                            : "hover:bg-[#4B3F72] hover:text-[#EAF2EF] dark:hover:bg-[#B794F4] dark:hover:text-[#2A2A2D]"
+                        }`
+                      }
+                    >
+                      <FontAwesomeIcon className="mr-2" icon={faGear} />
+                      Settings
+                    </NavLink>
+                  </li>
+                  <li className="border-t mt-2 border-[#D3D3D3] text-xl text-[#2E2E2E] dark:border-[#3C3C3F] dark:text-[#EAEAEA]">
+                    <button
+                      onClick={toggleTheme}
+                      className="w-full flex items-center px-4 py-2 text-lg transition-all duration-300 rounded-xl
+              hover:bg-[#4B3F72] hover:text-[#EAF2EF] dark:hover:bg-[#B794F4] dark:hover:text-[#2A2A2D]"
+                    >
+                      <FontAwesomeIcon
+                        className="mr-2 prevent-select "
+                        icon={faRightFromBracket}
+                      />
+                      Log out
+                    </button>
+                  </li>
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </section>
       </aside>
     </>
